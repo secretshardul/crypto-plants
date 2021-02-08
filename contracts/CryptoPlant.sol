7f144e5151ca0213075d638603d0c0c809bfbe02
shardul <shardul.aeer@somaiya.edu>
2021-02-08 19:44:54 +0530

    feat: Transfer share to charity

--- contracts/CryptoPlant.sol
+++ contracts/CryptoPlant.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.7;
 import "../client/node_modules/@openzeppelin/contracts/presets/ERC721PresetMinterPauserAutoId.sol";
 
 contract CryptoPlant is ERC721PresetMinterPauserAutoId {
-    uint256 public seedCost = 500;
+    address payable public petaAddress;
 
     constructor()
         ERC721PresetMinterPauserAutoId(
@@ -12,15 +12,15 @@ contract CryptoPlant is ERC721PresetMinterPauserAutoId {
             "CPT",
             "https://my-json-server.typicode.com/abcoathup/samplenft/tokens/"
         )
-    {}
-
-    // function purchaseSeed(uint256 x) public {
-    //     // require(msg.value == seedCost);
-    // }
+    {
+        petaAddress = 0x42F323c617c0a6d18547B8a2AaF8BcD1Abe617c9;
+    }
 
     receive() external payable {}
 
     function purchaseSeed() public payable {
         require(msg.value == 0.1 ether);
+
+        petaAddress.transfer(0.09 ether);
     }
 }
