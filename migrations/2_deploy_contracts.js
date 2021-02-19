d466b917f808c2a49f51137cad3df72f2e0fc0e1
shardul <shardul.aeer@somaiya.edu>
2021-02-19 18:53:51 +0530

    refactor: Remove simple storage contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-const SimpleStorage = artifacts.require("./SimpleStorage.sol")
 const CryptoPlant = artifacts.require("./CryptoPlant.sol")
 
 module.exports = function (deployer) {
-  deployer.deploy(SimpleStorage)
   deployer.deploy(CryptoPlant)
 }
